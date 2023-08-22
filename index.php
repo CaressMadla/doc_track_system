0c41f6cc73cee9a06d29487fbd2a5bf920f9247e
Erwin Ibarra <eibarra@iripple.com>
2023-08-22 14:06:03 +0800

    add bs5

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<<<<<<< HEAD
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css"
@@ -36,29 +35,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
 
   </body>
-=======
-    <?php
-    // Include the database connection
-    include 'connection.php';
-
-    // Query to retrieve data from the database
-    $sql = "SELECT id, name, email FROM users";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<ul>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>ID: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "No records found.";
-    }
-
-    // Close the connection (Note: It's often not necessary to manually close the connection)
-    $conn->close();
-    ?>
-</body>
->>>>>>> 6c594a50226f5679048f58637a73dde67f7fdf1a
 </html>
 
